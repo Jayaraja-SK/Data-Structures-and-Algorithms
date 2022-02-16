939e6f8fbb4bbcac373bac3b1c8cf68bae086132
Jayaraja S.K <jayaraja19041@cse.ssn.edu.in>
2022-02-16 15:36:33 +0530

    Update BST.py

--- Data_Structures/BST.py
+++ Data_Structures/BST.py
@@ -1,203 +1,200 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
+class BST:
+    def __init__(self,init_val=None):
+        self.value=init_val
+
+        if(self.value is None):
+            self.left=None
+            self.right=None
+        else:
+            self.left=BST()
+            self.right=BST()
+
+
+    def is_empty(self):
+        if(self.value is None):
+            return True
+
+        return False
+
+
+    def Insert(self,v):
+        if(self.is_empty()):
+            self.value=v
+            self.left=BST()
+            self.right=BST()
+
+            return
+
+        if(self.value==v):
+            return
+        elif(v<self.value):
+            self.left.Insert(v)
+            return 
+        else:
+            self.right.Insert(v)
+            return
+
+
+    def Inorder(self): # LEFT, NODE, RIGHT
+        if(self.is_empty()):
+            return ([])
+        else:
+            return (self.left.Inorder()+[self.value]+self.right.Inorder())
+
+
+    def Preorder(self): # NODE, LEFT, RIGHT
+        if(self.is_empty()):
+            return ([])
+        else:
+            return ([self.value]+self.left.Preorder()+self.right.Preorder())
+
+
+    def Postorder(self): # LEFT, RIGHT, NODE
+        if(self.is_empty()):
+            return ([])
+        else:
+            return (self.left.Postorder()+self.right.Postorder()+[self.value])
+
+
+    def Min_Value(self):
+        if(self.left.is_empty()):
+            return self.value
+        else:
+            return self.left.Min_Value()
+
+
+    def Max_Value(self):
+        if(self.right.is_empty()):
+            return self.value
+        else:
+            return self.right.Max_Value()
+
+
+    def is_leaf(self): # CHECKS IF CURRENT NODE IS LEAF NODE OR NOT
+        return (self.value!=None and self.left.is_empty() and self.right.is_empty())
+
+
+    def Clear_Node(self): # DELETES LEAF NODE
+        self.value=None
         self.left=None
         self.right=None
 
-    def set_data(self,data):
-        self.data=data
 
-    def get_data(self):
-        return self.data
+    def Copy_Left(self):
+        self.value=self.left.value
+        self.left=self.left.left
+        self.right=self.left.right
 
-    def set_left(self,node):
-        self.left=node
 
-    def get_left(self):
-        return self.left
+    def Copy_Right(self):
+        self.value=self.right.value
+        self.left=self.right.left
+        self.right=self.right.right
 
-    def set_right(self,node):
-        self.right=node
 
-    def get_right(self):
-        return self.right
+    def Delete(self,v):
+        if(self.is_empty()):
+            return
+        if(v<self.value):
+            self.left.Delete(v)
+        elif(v>self.value):
+            self.right.Delete(v)
+        elif(v==self.value):
+            if(self.is_leaf()): # NODE IS LEAF
+                self.Clear_Node()
+            elif(self.left.is_empty()): # LEFT NODE IS EMPTY
+                self.Copy_Right()
+            elif(self.right.is_empty()): # RIGHT NODE IS EMPTY
+                self.Copy_Left()
+            else: # BOTH LEFT AND RIGHT NODES ARE NOT EMPTY
+                self.value=self.left.Max_Value()
+                self.left.Delete(self.left.Max_Value())
+
+                
     
+'''
+obj=BST()
+obj.Insert(1)
+obj.Insert(2)
+obj.Insert(-1)
+obj.Insert(3)
+obj.Insert(4)
+print()
+print(obj.Postorder())
+print()
+#obj.Min_Value()
+#obj.Max_Value()
+print(obj.Delete(10))
 
-class BST:
-    def __init__(self,data=None):
-        if(data is None):
-            self.root=None
-        else:
-            self.root=data
-        
-    def insert(self,data):
-        if(self.root is None):
-            self.root=Node(data)
-        else:
-            if(self.root.data > data):
-                if(self.root.get_left() is not None):
-                    BST(self.root.get_left()).insert(data) #REFERENCES TO NEW SUB-TREE
-                else:
-                    self.root.set_left(Node(data))
-            if(self.root.data < data):
-                if(self.root.get_right() is not None):
-                    BST(self.root.get_right()).insert(data)
-                else:
-                    self.root.set_right(Node(data))
-        
-    def search(self,data,level):
-        if(self.root is not None):
-            if(self.root.data == data):
-                print(f'\nELEMENT FOUND AT LEVEL = {level}')
-            elif(self.root.data > data):
-                if(self.root.get_left() is None):
-                    print(f'\nITEM NOT FOUND')
-                else:
-                    BST(self.root.get_left()).search(data,level+1)
-            elif(self.root.data < data):
-                if(self.root.get_right() is None):
-                    print(f'\nITEM NOT FOUND')
-                else:
-                    BST(self.root.get_right()).search(data,level+1)
-        else:
-            print(f'\nITEM NOT FOUND')
+print()
+obj.Inorder()
+print()
+'''
 
-    def delete(self,data):
-        if(self.root is None):
-            return None
-        if(self.root.data > data):
-            if(self.root.get_left() is not None):
-                self.root.set_left(BST(self.root.get_left()).delete(data))
-        elif(self.root.data < data):
-            if(self.root.get_right() is not None):
-                self.root.set_right(BST(self.root.get_right()).delete(data))
-        else:
-            if(self.root.get_left() and self.root.get_right()):
-                left_max=BST(self.root.get_left()).find_max().get_data()
-                self.root.set_data(left_max)
-                self.root.set_left(BST(self.root.get_left()).delete(left_max))
-            elif(self.root.get_left()):
-                self.root=self.root.get_left()
-            elif(self.root.get_right()):
-                self.root=self.root.get_right()
-            else:
-                self.root=None
 
-        return self.root
-
-    def find_max(self):
-        if(self.root is None):
-            return None
-
-        if(self.root.get_right() is None):
-            return self.root
-        else:
-            return BST(self.root.get_right()).find_max()
-
-    def find_min(self):
-        if(self.root is None):
-            return None
-
-        if(self.root.get_left() is None):
-            return self.root
-        else:
-            return BST(self.root.get_left()).find_min()
-
-    def preorder(self): # PARENT-LEFT-RIGHT
-        print(self.root.data)
-
-        if(self.root.get_left() is not None):
-            BST(self.root.get_left()).preorder()
-
-        if(self.root.get_right() is not None):
-            BST(self.root.get_right()).preorder()            
-
-    def inorder(self): # LEFT-PARENT-RIGHT
-        if(self.root.get_left() is not None):
-            BST(self.root.get_left()).inorder()
-
-        print(self.root.data)
-        
-        if(self.root.get_right() is not None):
-            BST(self.root.get_right()).inorder()
-            
-    def postorder(self): # LEFT-RIGHT-PARENT
-        if(self.root.get_left() is not None):
-            BST(self.root.get_left()).postorder()
-
-        if(self.root.get_right() is not None):
-            BST(self.root.get_right()).postorder()
-
-        print(self.root.data)
-
-        
-            
 
 if __name__=="__main__":
-    
-    print("-"*60)
-    print(f'\t\t\tBINARY SEARCH TREE')
-    print("-"*60)
-    print()
-
     obj=BST()
     
-    choice=0
     while(True):
-        print("-"*50)
-        print("\t\t\tMENU")
-        print("-"*50)
-        print("1. INSERT")
-        print("2. SEARCH")
-        print("3. DELETE")
-        print("4. FIND MIN")
-        print("5. FIND MAX")
-        print("6. PRE-ORDER TRAVERSAL")
-        print("7. IN-ORDER TRAVERSAL")
-        print("8. POST-ORDER TRAVERSAL")
-        print("9. EXIT\n")
-        choice=int(input("ENTER CHOICE="))
+        print("-"*40)
+        print(f'\t\tMENU')
+        print("-"*40)
+        print(f'| 1. | INSERT')
+        print(f'| 2. | DELETE')
+        print(f'| 3. | MIN. VALUE')
+        print(f'| 4. | MAX. VALUE')
+        print(f'| 5. | IN-ORDER')
+        print(f'| 6. | PRE-ORDER')
+        print(f'| 7. | POST-ORDER')
+        print(f'| 8. | EXIT')
+        print("-"*40)
         print()
-        if choice==1:
-            val=int(input("ENTER ITEM TO BE INSERTED="))
 
-            obj.insert(val)
+        choice=int(input("ENTER CHOICE = "))
+        print()
 
-            print(f'\nITEM INSERTED SUCCESSFULLY')
+        if(choice==1):
+            val=int(input("ENTER VALUE TO BE INSERTED = "))
+            print()
 
-        elif choice==2:
-            val=int(input("ENTER ITEM TO BE SEARCHED="))
-            
-            obj.search(val,0)
+            obj.Insert(val)
 
-        elif choice==3:
-            val=int(input("ENTER ITEM TO BE DELETED="))
+            print(f'VALUE IS INSERTED\n')
+        elif(choice==2):
+            val=int(input("ENTER VALUE TO BE DELETED = "))
+            print()
 
-            obj.delete(val)
+            obj.Delete(val)
+        elif(choice==3):
+            print(f'MIN. VALUE IN TREE = {obj.Min_Value()}\n')
+        elif(choice==4):
+            print(f'MAX. VALUE IN TREE = {obj.Max_Value()}\n')
+        elif(choice==5):
+            res=obj.Inorder()
 
-            print(f'\nITEM DELETED SUCCESSFULLY')
+            print(f'IN-ORDER TRAVERSAL')
 
-        elif choice==4:
-            print(f'MINIMUM ELEMENT IN TREE = {obj.find_min().data}')
+            for i in res:
+                print(f'{i}')
+            print()
+        elif(choice==6):
+            res=obj.Preorder()
 
-        elif choice==5:
-            print(f'MAXIMUM ELEMENT IN TREE = {obj.find_max().data}')
+            print(f'PRE-ORDER TRAVERSAL')
 
-        elif choice==6:
-            obj.preorder()
+            for i in res:
+                print(f'{i}')
+            print()
+        elif(choice==7):
+            res=obj.Postorder()
 
-        elif choice==7:
-            obj.inorder()
+            print(f'POST-ORDER TRAVERSAL')
 
-        elif choice==8:
-            obj.postorder()
-
-        elif choice==9:
+            for i in res:
+                print(f'{i}')
+            print()
+        elif(choice==8):
             break
-
         else:
-            print("INVALID CHOICE")
-
-        print("\n")
-        
-        
+            print(f'INVALID CHOICE')
